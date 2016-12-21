c016f13924ab06508cf77d190ba1dd0532b30a31
asbsoft <ab2014box@gmail.com>
2016-12-21 21:47:30 +0200

    Module's uniqueId now can be send to Bootstrap class as parameter 'moduleUid' (need if module deep nested)

--- Bootstrap.php
+++ Bootstrap.php
@@ -16,35 +16,45 @@ use asb\yii2\modules\restapi_v0\models\User;
 class Bootstrap implements BootstrapInterface
 {
     public $urlPrefix = 'restapi-v0'; // default
-    public $moduleId;
+    public $moduleUid;
 
     public function bootstrap($app)
     {
-        $paramsHere = include(__DIR__ . '/config/params.php');
+        $paramsHere = include(__DIR__ . '/config/params.php');//var_dump($paramsHere);
         $params = [];
-        foreach($app->modules as $moduleId => $module) {
-            if (is_array($module) && isset($module['class']) && $module['class'] == Module::className()) {
-                $params = $module['params'];
-                $this->moduleId = $moduleId;
-                break;
-            } else if ($module instanceof Module) {
-                $params = $module->params;
-                $this->moduleId = $moduleId;
-                break;
+
+        if (empty($this->moduleUid)) {
+            // find only in 1st-level submodules of application:
+            foreach($app->modules as $moduleId => $module) {//echo"<br>{$moduleId}:";if($module instanceof \yii\base\Module)echo"{$module->uniqueId}";else var_dump($module);
+                if (is_array($module) && isset($module['class']) && $module['class'] == Module::className()) {
+                    $params = $module['params'];
+                    $this->moduleUid = $moduleId; //??
+                    break;
+                } else if ($module instanceof Module) {
+                    $params = $module->params;
+                    $this->moduleUid = $module->module->uniqueId . '/' . $moduleId;
+                    break;
+                }
             }
         }
-        if (empty($this->moduleId)) throw new \Exception('Not found in configuration: module ' . Module::className());
         $params = ArrayHelper::merge($paramsHere, $params);
         $this->urlPrefix = empty($params['urlPrefix']) ? $this->urlPrefix : $params['urlPrefix'];
 
+        if (empty($this->moduleUid)) {
+            $message = "Not found 'moduleUid' in configuration of module " . Module::className();//
+            echo "{$message}<br>";
+            //throw new \Exception($message);
+            Yii::error($message);
+            return;
+        }
+
         if (!empty($params['changeStartPage']) && $params['changeStartPage']) {
-            Yii::$app->defaultRoute = $this->moduleId . '/frontend/auth';
+            Yii::$app->defaultRoute = $this->moduleUid . '/frontend/auth';
         }
 
         Yii::$app->urlManager->enablePrettyUrl = true;
-
         $urlPrefix = $this->urlPrefix;
-        $moduleId  = $this->moduleId;
+        $moduleUid  = $this->moduleUid;
         $routes = include(__DIR__ . '/config/routes-front.php');
         Yii::$app->urlManager->addRules($routes, false);
         //var_dump(Yii::$app->urlManager->rules[2]->rules['posts']);
--- config/routes-front.php
+++ config/routes-front.php
@@ -1,28 +1,28 @@
 <?php
 /**
  * @var $urlPrefix string
- * @var $moduleId string 
+ * @var $moduleUid string 
  */
 return [
 /*
     'front' => [
         'class'   => 'yii\web\UrlRule',
         'pattern' => $urlPrefix . '/<action:(auth|captcha)>',
-        'route'   => $moduleId . '/frontend/<action>',
+        'route'   => $moduleUid . '/frontend/<action>',
     ],
 */
-    "{$urlPrefix}/<action:(auth|captcha)>" => "{$moduleId}/frontend/<action>",
-    "{$urlPrefix}/?" => "{$moduleId}/frontend/auth",
+    "{$urlPrefix}/<action:(auth|captcha)>" => "{$moduleUid}/frontend/<action>",
+    "{$urlPrefix}/?" => "{$moduleUid}/frontend/auth",
 
     'posts' => [
         'class' => 'yii\rest\UrlRule',
-        'controller' => ['posts' => $moduleId . '/post'],
+        'controller' => ['posts' => $moduleUid . '/post'],
         'prefix' => $urlPrefix,
         //'suffix' => '.json',
     ],
     'users' => [
         'class' => 'yii\rest\UrlRule',
-        'controller' => ['users' => $moduleId . '/user'],
+        'controller' => ['users' => $moduleUid . '/user'],
         'prefix' => $urlPrefix,
         //'suffix' => '.json',
     ],
