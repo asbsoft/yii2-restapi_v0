c172934db1f06623d621da8f7069c7154f4f0965
asbsoft <ab2014box@gmail.com>
2018-01-25 19:53:08 +0200

    fix bug in migration windows bat-file: pass wrong parameter

--- migrations/migrate.bat
+++ migrations/migrate.bat
@@ -1,5 +1,17 @@
 @echo off
-set APP=../../../../..
+
+set APP=../../../../../yii.bat
+if exist %APP% goto START
+
+set APP=../../../../yii.bat
+if exist %APP% goto START
+
+echo Yii console bootstrap file not found
+goto DONE
+
+:START
 set CMD=up
 if not '%1'=='' set CMD=%1
-call %APP%/yii.bat migrate/%CMD% --migrationPath=.
+call %APP% migrate/%CMD% "--migrationPath=." %2 %3 %4 %5 %6 %7 %8 %9
+:DONE
+
